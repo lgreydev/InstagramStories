474bbf92a90e8849b3e5c5ecde5216463870fb27
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-09-29 11:10:45 +0300

    Added images

--- InstagramStories/StoryDisplay.swift
+++ InstagramStories/StoryDisplay.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct StoryDisplay: View {
+
+    var imageNames:[String] = ["image01", "image02", "image03", "image04", "image05",]
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
