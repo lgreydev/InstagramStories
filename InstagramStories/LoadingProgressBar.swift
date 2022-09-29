9970bbf1dff9145c5d4c687526a1a10ec286a35c
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-09-29 10:40:03 +0300

    Added progress bar

---
+++ InstagramStories/LoadingProgressBar.swift
@@ -0,0 +1,38 @@
+//
+//  LoadingProgressBar.swift
+//  InstagramStories
+//
+//  Created by Sergey Lukaschuk on 29.09.2022.
+//
+
+import SwiftUI
+
+struct LoadingProgressBar: View {
+
+    var progress: CGFloat
+
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .foregroundColor(.white.opacity(05))
+                    .cornerRadius(5)
+                Rectangle()
+                    .frame(width: geometry.size.width * self.progress,
+                           height: nil,
+                           alignment: .leading)
+                    .foregroundColor(.white)
+                    .cornerRadius(5)
+            }
+        }
+    }
+}
+
+
+
+
+struct LoadingProgressBar_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingProgressBar(progress: 0.1)
+    }
+}
