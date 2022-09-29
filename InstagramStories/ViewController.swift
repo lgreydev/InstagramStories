9f572de9ad660947625645ad78623aee7a6d3846
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-09-29 10:42:58 +0300

    Added button

--- InstagramStories/ViewController.swift
+++ InstagramStories/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
+    private let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupButton()
     }
-
-
 }
 
+private extension ViewController {
+    func setupButton() {
+        view.addSubview(button)
+        button.center = view.center
+        button.setTitle("Show Settings", for: .normal)
+        button.backgroundColor = .systemIndigo
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+
+    @objc func didTapButton() {
+        let vc = UIHostingController(rootView: LoadingProgressBar(progress: 0.1))
+        present(vc, animated: true)
+    }
+}
